ff74db1cea36591dd001f10006e3e9e58cfe800f
Ramiro <castilloramiro313@gmail.com>
2018-10-10 13:37:41 -0400

    Añandiendo Archivos de Config

---
+++ website/code/assest/js/main.js
@@ -0,0 +1,44 @@
+	$(window).scroll(function(){
+		if($(window).scrollTop()>100){
+			$("#navsticky").attr("class","w-100 navbar sticky-top navbar-expand-lg px-5");
+		}
+	});
+	$(window).scroll(function(){
+		if($(window).scrollTop()<=100){
+			$("#navsticky").attr("class","my-nav w-100 navbar navbar-expand-lg px-5");
+		}
+	});
+
+	$(".navbar").slideDown(3000);
+
+
+
+
+
+function testAnim(x) {
+	$('.modal .modal-dialog').attr('class', 'modal-dialog  modal-dialog-centered  ' + x + ' animated');
+};
+$('#myModal').on('show.bs.modal', function (e) {
+	var anim = "zoomIn";
+	testAnim(anim);
+});
+$('#myModal').on('hide.bs.modal', function (e) {
+	var anim = "rollOut";
+  	testAnim(anim);
+});
+$('#myModalEntrar').on('show.bs.modal', function (e) {
+	var anim = "zoomIn";
+	testAnim(anim);
+});
+$('#myModalEntrar').on('hide.bs.modal', function (e) {
+	var anim = "rollOut";
+  testAnim(anim);
+});
+$('#menuModal').on('show.bs.modal', function (e) {
+	var anim = "zoomIn";
+	testAnim(anim);
+});
+$("#menuModal").on('hide.bs.modal', function (e) {
+	var anim = "rollOut";
+  testAnim(anim);
+});
